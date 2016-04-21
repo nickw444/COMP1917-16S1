4c13288d4e79d3eb68ff91939166e7cba815602f
Nicholas Whyte <nick@nickwhyte.com>
2016-04-21 10:37:24 +1000

    Week07

--- tuts/07/reverse.c
+++ tuts/07/reverse.c
@@ -1,46 +1,31 @@
-//By:          Nick Whyte
-//Course:      COMP1917
-//Date:        18 April
-//Tutorial:    Wednesday Flute 3pm-6pm.
-//Tutor:       Jackie
-//Description: This function will reverse a string
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
-
 char *reverse (char *message);
-
 
 int main(int argc, char const *argv[])
 {
-	char * string = "Hello Theree";
-	char * newString = reverse(string);
-	printf("%s\n", newString);
+    char * string = "Hello There";
+    char * newString = reverse(string);
+    printf("String is: %s\n", newString);
 
-	int i = 0;
-	while(newString[i] != 0) {
-		printf("%c\n", newString[i]);
-		i++;
-	}
-	printf("%d\n", newString[i]);
-		
-	free(newString);
+    free(newString);
 
-	return EXIT_SUCCESS;
+    return EXIT_SUCCESS;
 }
 
 char *reverse (char *message) {
-	int memorySize = strlen(message) * sizeof(char);
-	char *newString = malloc(memorySize);
+    int i = 0;
+    int bufLength = strlen(message) + 1;
+    char * newString = malloc(sizeof(char) * (bufLength));
 
-	int messageLength = strlen(message);
-	int counter = messageLength - 1;
-	int index = 0;
-	while(counter >= 0) {
-		newString[index] = message[counter];
-		index ++;
-		counter --;
-	}
-	newString[index] = 0;
-	return newString;
+    newString[bufLength - 1] = 0;
+    int j = bufLength - 2;
+
+    while (i < (bufLength -1)) {
+        newString[j] = message[i];
+        i ++;
+        j --;
+    }
+    return newString;
 }
