c44287ab754ab9c95e969e8c852bbc1276d9f399
Nick Whyte <nick@nickwhyte.com>
2016-05-11 20:24:49 +1000

    Add stack ADT broken constructors

--- tuts/10/stack_by_reference/stack.c
+++ tuts/10/stack_by_reference/stack.c
@@ -24,6 +24,16 @@ Stack newStack (void) {
     //allocate memory on heap to store the stack
 }
 
+Stack newStackBroken1 (void) {
+    stack s;
+    return &s;
+}
+
+Stack newStackBroken2 (void) {
+    Stack s = malloc(sizeof(Stack))
+    return s;
+}
+
 //free the memory allocated on heap for the stack
 void freeStack (Stack s){
     // Implement Me
