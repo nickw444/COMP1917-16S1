5c9d45c0a8fa9c750fdd9aae58fc586246f727f0
Nick Whyte <nick@nickwhyte.com>
2016-05-26 11:24:05 +1000

    Create sample_solution.c

---
+++ pracs/prac7/sample_solution.c
@@ -0,0 +1,79 @@
+// frontBackSplit.c
+// 
+ 
+#include <stdlib.h>
+#include <stdio.h>
+#include <assert.h>
+ 
+#include "list-ppexam.h"
+
+// given a list of nodes, split it into two sublists 
+// - one for the front half, and one for the back half. 
+// If the number of elements is odd, the extra element 
+// should go in the front list. 
+
+// The first argument (sourceList) is the original list.
+// The second argument (frontList) which is initally empty 
+// should point to the front half of the list.
+// The third argument (backList) which is initially empty
+// should point to the back half of the list.
+// The original list (sourceList) should be empty after the split.
+//
+// Constraints:
+// don't malloc any new nodes
+// don't change the "value" field of any node, just change links
+// don't delete any nodes (i.e. do not call free())
+
+// e.g. if sourceList is 1->4->6->4->3->X then 
+// after execution of the function frontBackSplit() 
+// frontList should be 1->4->6->X and 
+// backList should be 4->3->X and
+// sourceList should be X (i.e. empty)
+//
+// and if sourceList is 3->10->15->25->X then
+// after execution of the function frontBackSplit() 
+// frontList should be 3->10->X and
+// backList should be 15->25->X
+// sourceList should be X (i.e. empty)
+
+// A function to determine the length of a list called numItems() 
+// is provided. Fell free to use it but do not modify the function.
+
+//function to determine length of a linked list
+int numItems (list l) {
+
+    int length = 0;
+    link current = l->head;
+    while (current != NULL){
+        length++;
+        current = current->next;
+    }
+    return length;
+}
+
+void frontBackSplit (list sourceList, list frontList, list backList) {
+    int listLength = numItems(sourceList);
+    int i = 0;
+    int mid = listLength / 2;
+    
+    link curr = sourceList->head;
+    // Set up frontList to point to the start.
+    frontList->head = sourceList->head;
+    
+    // Handle odd case differently.
+    if (listLength % 2 == 0) {
+        mid = mid - 1;
+    }
+
+    while (i < mid) {
+        // Traverse the list until we get to the midpoint
+        curr = curr->next;
+        i ++;
+    }
+    if (curr != NULL) {
+        backList->head = curr->next;
+        curr->next = NULL;
+    }
+
+    sourceList->head = NULL;
+}
